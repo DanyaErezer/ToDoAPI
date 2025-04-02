ddf71da1160220e125cef4df8e0b3bbd6d4dd08a
DanyaErezer <pavlov.shura12@gmail.com>
2025-04-02 13:54:42 +0700

    first commit

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\RouteServiceProvaider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\RouteServiseProvaider::class,
+];
